6edffc8dbc2a16eaef555ba8bdf91278db71183e
tflynt91 <flynt.tyler@gmail.com>
2020-11-19 09:51:14 -0500

    Score Manager

---
+++ ScoreManager/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScoreManager.Controllers;
+
+
+namespace ScoreManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoreController scoreController = new ScoreController();
+            scoreController.Run();
+        }
+    }
+}
