9fbd332c2232072fa2d6192a0fd273d66b01b264
tflynt91 <flynt.tyler@gmail.com>
2020-11-28 19:24:20 -0500

    re-added UI to project

---
+++ GuildCars/GuildCars.Tests/GuildCars.UI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using GuildCars.Data;
+using GuildCars.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuildCars.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repo = new VehicleInventoryRepository();
+            HomeViewModel homeView = new HomeViewModel();
+            homeView.Specials = repo.GetSpecials();
+            homeView.FeaturedVehicles = repo.GetFeaturedVehicles();
+            return View(homeView);
+        }
+
+       
+    }
+}
---
+++ GuildCars/GuildCars.UI/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GuildCars.Models.Queries;
+using GuildCars.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuildCars.UI.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Special> Specials { get; set; }
+        public IEnumerable<VehicleShortItem> FeaturedVehicles { get; set; }
+
+    }
+}
